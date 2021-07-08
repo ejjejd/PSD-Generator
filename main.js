8bcaf4dbb99a6657ad36301d0b830648e8026075
Petro <ptrstasiv@gmail.com>
2021-07-08 17:03:06 +0300

    Samples limit

--- main.js
+++ main.js
@@ -5,7 +5,11 @@ var rejectAttempts = 30;
 
 var sampleSize = minRadius / Math.sqrt(2);
 
+var samplesLimit = 0;
+
 var gridSize = Math.floor(canvasSize / sampleSize);
+
+var currentSamples;
 
 var grid = [];
 var activeList = [];
@@ -13,7 +17,6 @@ var activeList = [];
 var canvasCtx;
 
 var updateFuncHandle;
-
 
 class Vector2D {
     constructor(_x, _y) {
@@ -42,9 +45,10 @@ function random(min, max) {
 function setup() {
     createCanvas(canvasSize);
 
-    document.getElementById('sizeSlider').value = canvasSize;
-    document.getElementById('radiusSlider').value = minRadius;
-    document.getElementById('attemptsSlider').value = rejectAttempts;
+    document.getElementById('sizeInput').value = canvasSize;
+    document.getElementById('radiusInput').value = minRadius;
+    document.getElementById('attemptsInput').value = rejectAttempts;
+    document.getElementById('limitInput').value = samplesLimit;
 }
 
 function startPSD() {
@@ -53,6 +57,8 @@ function startPSD() {
 
     activeList = [];
 
+    currentSamples = 1;
+
     const randomX = random(0, canvasSize);
     const randomY = random(0, canvasSize);
 
@@ -60,6 +66,63 @@ function startPSD() {
     activeList.push(grid[0]);
 
     startUpdate();
+}
+
+function psdStep() {
+    var randId = Math.floor(random(0, activeList.length));
+    var sample = activeList[randId];
+
+    var found = false;
+
+    for(var i = 0; i < rejectAttempts; ++i) {
+        var newSample = randomVector2D();
+        var radius = random(minRadius, 2 * minRadius);
+        newSample.x = sample.x + newSample.x * radius;
+        newSample.y = sample.y + newSample.y * radius;
+
+        var row = Math.floor(newSample.x / sampleSize);
+        var col = Math.floor(newSample.y / sampleSize);
+
+        if(grid[row + col * gridSize])
+            continue;
+
+        if(newSample.x < 0 || newSample.x > canvasSize
+           || newSample.y < 0 || newSample.y > canvasSize) {
+            continue;
+        }
+            
+        var pass = true;
+
+        for(var x = -1; x <= 1; ++x) {
+            for(var y = -1; y <= 1; ++y) {
+                var idX = row + x;
+                var idY = col + y;
+
+                var near = grid[idX + idY * gridSize];
+
+                if(near) {
+                    var d = distVector2D(newSample, near);
+                    if(d < minRadius)
+                       pass = false;
+                }
+
+            }
+        }
+
+        if(pass) {
+            found = true;
+
+            grid[row + col * gridSize] = newSample;
+            activeList.push(newSample)
+
+            ++currentSamples;
+
+            break;
+        }
+    }
+
+    if(samplesLimit == 0 && !found)
+        activeList.splice(randId, 1);
 }
 
 function createCanvas(size) {
@@ -78,9 +141,10 @@ function startUpdate() {
 
     document.getElementById('genButton').disabled = true;
     document.getElementById('clearButton').disabled = false;
-    document.getElementById('sizeSlider').disabled = true;
-    document.getElementById('radiusSlider').disabled = true;
-    document.getElementById('attemptsSlider').disabled = true;
+    document.getElementById('sizeInput').disabled = true;
+    document.getElementById('radiusInput').disabled = true;
+    document.getElementById('attemptsInput').disabled = true;
+    document.getElementById('limitInput').disabled = true;
 }
 
 function stopUpdate() {
@@ -88,68 +152,26 @@ function stopUpdate() {
 
     document.getElementById('genButton').disabled = false;
     document.getElementById('clearButton').disabled = true;
-    document.getElementById('sizeSlider').disabled = false;
-    document.getElementById('radiusSlider').disabled = false;
-    document.getElementById('attemptsSlider').disabled = false;
+    document.getElementById('sizeInput').disabled = false;
+    document.getElementById('radiusInput').disabled = false;
+    document.getElementById('attemptsInput').disabled = false;
+    document.getElementById('limitInput').disabled = false;
+}
+
+function updateCheck() {
+    var listLengthCheck = activeList.length > 0;
+    var samplesCheck =  currentSamples < samplesLimit && currentSamples < gridSize * gridSize;
+
+    return samplesLimit > 0 ? samplesCheck : listLengthCheck;
 }
 
 function updatePSD() {
-    if(activeList.length > 0) {
-        var randId = Math.floor(random(0, activeList.length));
-        var sample = activeList[randId];
-
-        var found = false;
-
-        for(var i = 0; i < rejectAttempts; ++i) {
-            var newSample = randomVector2D();
-            var radius = random(minRadius, 2 * minRadius);
-            newSample.x = sample.x + newSample.x * radius;
-            newSample.y = sample.y + newSample.y * radius;
-
-            var row = Math.floor(newSample.x / sampleSize);
-            var col = Math.floor(newSample.y / sampleSize);
-
-            if(grid[row + col * gridSize])
-                continue;
-
-            if(newSample.x < 0 || newSample.x > canvasSize
-               || newSample.y < 0 || newSample.y > canvasSize) {
-                continue;
-            }
-                
-            var pass = true;
-
-            for(var x = -1; x <= 1; ++x) {
-                for(var y = -1; y <= 1; ++y) {
-                    var idX = row + x;
-                    var idY = col + y;
-
-                    var near = grid[idX + idY * gridSize];
-
-                    if(near) {
-                        var d = distVector2D(newSample, near);
-                        if(d < minRadius)
-                           pass = false;
-                    }
-
-                }
-            }
-
-            if(pass) {
-                found = true;
-
-                grid[row + col * gridSize] = newSample;
-                activeList.push(newSample)
-
-                break;
-            }
-        }
-        
-        if(!found)
-            activeList.splice(randId, 1);
-    }
+    if(updateCheck())
+        psdStep();
     else
         stopUpdate();
+
+    console.log(samplesLimit);
 }
 
 function draw() {
@@ -176,9 +198,10 @@ function draw() {
 }
 
 function updateValues() {
-    var sizeValue = parseInt(document.getElementById('sizeSlider').value);
-    var radiusValue = parseInt(document.getElementById('radiusSlider').value);
-    var attemptsValue = parseInt(document.getElementById('attemptsSlider').value);
+    var sizeValue = parseInt(document.getElementById('sizeInput').value);
+    var radiusValue = parseInt(document.getElementById('radiusInput').value);
+    var attemptsValue = parseInt(document.getElementById('attemptsInput').value);
+    var samplesLimitValue = parseInt(document.getElementById('limitInput').value);
 
     createCanvas(sizeValue);
 
@@ -188,6 +211,8 @@ function updateValues() {
     rejectAttempts = attemptsValue;
 
     sampleSize = minRadius / Math.sqrt(2);
+
+    samplesLimit = samplesLimitValue;
 
     gridSize = Math.floor(canvasSize / sampleSize);
 }
